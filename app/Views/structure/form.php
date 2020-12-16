40fb3eb9ec57e1a7525a395ef6b7d244ed2aa31b
Jean <jean_wylmer@hotmail.com>
2020-12-16 18:17:27 -0300

    Adicionado a renderizaçao: label, div pai, div row com mais de um campo na linha

--- app/Views/structure/form.php
+++ app/Views/structure/form.php
@@ -1,7 +1,28 @@
-<form action="<?= $action ?>" method="post">
+<form action="<?= $action ?>" method="post" class="<?= implode(' ', $class_form ?? array()) ?>">
     <?php
-    foreach ($inputs as $infos) {
-        echo view('structure/input/__input', $infos);
+    foreach ($inputs as $row) { ?>
+        <div class="row">
+            <?php
+            foreach ($row['elements'] as $element) {
+                echo view('structure/input/__input', $element);
+            }
+            ?>
+        </div>
+    <?php
+    }
+
+    if (isset($cancel)) {
+        $attr_string = '';
+        foreach ($cancel['attr'] as $name => $value) {
+            $attr_string .= $name . '="' . $value . '" ';
+        }
+        echo '
+        <a href="' . $cancel['href'] . '" class="' . implode(' ', $cancel['class'] ?? array()) . '" ' . $attr_string . '>' . $cancel['text'] . '</a>';
+    }
+
+    if (isset($submit)) {
+        echo '
+        <input type="submit" class="' . implode(' ', $submit['class'] ?? array()) . '" value="' . $submit['text'] . '">';
     }
     ?>
 </form>
--- app/Views/structure/input/__input.php
+++ app/Views/structure/input/__input.php
@@ -1,8 +1,10 @@
 <!-- Recupera um array com as informações de cada tipo, para atribuir attrs especificos -->
 <?php include 'app/Views/structure/input/' . $type . '.php' ?>
-
-<input type="<?= $attr['type'] ?>" name="<?= $name ?>" class="<?= implode(' ', $class) ?>" <?php
-                                                                                            foreach ($attr['others'] as $name => $value) {
-                                                                                                echo $name . '="' . $value . '" ';
-                                                                                            }
-                                                                                            ?> />
+<div class="<?= implode(' ', $class_div ?? array()) ?>">
+    <label for=" <?= $name ?>" class="<?= implode(' ', $class_label ?? array()) . implode(' ', $attr_label['class'] ?? array()) ?>"><?= $name_ui ?></label>
+    <input type="<?= $attr['type'] ?>" name="<?= $name ?>" class="<?= implode(' ', $class) ?>" <?php
+                                                                                                foreach ($attr['others'] as $name => $value) {
+                                                                                                    echo $name . '="' . $value . '" ';
+                                                                                                }
+                                                                                                ?> />
+</div>
